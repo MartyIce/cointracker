3f236b61a3620b6dbb18f4ebc6d5d0e66ab81937
Marty Mavis <marty.mavis@gmail.com>
2016-04-15 22:58:01 -0500

    updating code to accept full state name

--- app/controllers/coin_entries_controller.rb
+++ app/controllers/coin_entries_controller.rb
@@ -87,7 +87,65 @@ class CoinEntriesController < ApplicationController
   end
 
   def coords_for_city(c) 
-    key = c[:city] + "," + c[:state] + "," + c[:country];
+
+    states = {
+      "Alabama" => "AL",
+      "Alaska" => "AK",
+      "Arizona" => "AZ",
+      "Arkansas" => "AR",
+      "California" => "CA",
+      "Colorado" => "CO",
+      "Connecticut" => "CT",
+      "Delaware" => "DE",
+      "Florida" => "FL",
+      "Georgia" => "GA",
+      "Hawaii" => "HI",
+      "Idaho" => "ID",
+      "Illinois" => "IL",
+      "Indiana" => "IN",
+      "Iowa" => "IA",
+      "Kansas" => "KS",
+      "Kentucky" => "KY",
+      "Louisiana" => "LA",
+      "Maine" => "ME",
+      "Maryland" => "MD",
+      "Massachusetts" => "MA",
+      "Michigan" => "MI",
+      "Minnesota" => "MN",
+      "Mississippi" => "MS",
+      "Missouri" => "MO",
+      "Montana" => "MT",
+      "Nebraska" => "NE",
+      "Nevada" => "NV",
+      "New Hampshire" => "NH",
+      "New Jersey" => "NJ",
+      "New Mexico" => "NM",
+      "New York" => "NY",
+      "North Carolina" => "NC",
+      "North Dakota" => "ND",
+      "Ohio" => "OH",
+      "Oklahoma" => "OK",
+      "Oregon" => "OR",
+      "Pennsylvania" => "PA",
+      "Rhode Island" => "RI",
+      "South Carolina" => "SC",
+      "South Dakota" => "SD",
+      "Tennessee" => "TN",
+      "Texas" => "TX",
+      "Utah" => "UT",
+      "Vermont" => "VT",
+      "Virginia" => "VA",
+      "Washington" => "WA",
+      "West Virginia" => "WV",
+      "Wisconsin" => "WI",
+      "Wyoming" => "WY"
+    }
+
+    state = c[:state];
+    if(!states.has_value?(state))
+      state = states[:state];
+
+    key = c[:city] + "," + state + "," + c[:country];
     Rails.cache.fetch(key, expires_in: 12.hours) do
       MultiGeocoder.geocode(key)
     end
@@ -97,7 +155,7 @@ class CoinEntriesController < ApplicationController
     render json: CoinEntry.where("serial_number=?", params[:id].to_i.to_s.rjust(3, '0')).sort_by{|c| c[:created_at]}
   end
 
-  def find_last_for_each
+  def find_last_for_each    
     byCity = CoinEntry.find_by_sql "SELECT serial_number, max(id) as id from coin_entries group by serial_number"
     ids = []
     byCity.each do |bc|
